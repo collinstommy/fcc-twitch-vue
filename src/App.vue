34a63e65c9692897c0b7f1ac1c6cf83f7cda6c63
Thomas <tomascollins@gmail.com>
2017-10-22 13:09:28 +0100

    add src fles

---
+++ src/App.vue
@@ -0,0 +1,184 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 col-xs-12">
+          <div class="row status_icons">
+            <h2 class="text-center">TwitchTv Channels</h2>
+            <hr>
+            <div class="col-md-4 col-xs-4 centered both">
+              <span class="fa-stack fa-3x"  @click="show='all'">
+                <i class="fa fa-television fa-2x fa-stack-2x"></i>
+                <strong class="fa-stack-1x status-text">ALL</strong>
+              </span>
+            </div>
+            <div class="col-md-4 col-xs-4 centered online">
+              <span class="fa-stack fa-3x" @click="show='online'">
+                <i class="fa fa-television fa-2x fa-stack-2x"> </i>
+                <strong class="fa-stack-1x status-text">Online</strong>
+              </span>
+            </div>
+            <div class="col-md-4 col-xs-4 centered offline">
+              <span class="fa-stack fa-3x centered" @click="show='offline'">
+                <i class="fa fa-television fa-2x fa-stack-2x"></i>
+                <strong class="fa-stack-1x status-text">Offline</strong>
+              </span>
+            </div>
+          </div>
+          <hr />
+        </div>
+        <!-- stautus sections -->
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <div class="row">
+              <section id="streams" class="section">
+                <ChannelItem v-for="(channel, index) in visibleChannels" :channel="channel" :key="index"></ChannelItem>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+
+        </div>
+
+      </div>
+      <!-- streams -->
+    </div>
+    <!-- container -->
+
+  </div>
+</template>
+
+<script>
+
+import ChannelItem from './components/ChannelItem'
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  components: {
+    'ChannelItem': ChannelItem
+  },
+  data() {
+    return {
+      channels: [],
+      show: 'online',
+    }
+  },
+  computed: {
+    visibleChannels: function(){
+      return this.channels.filter(channel =>{
+          if(this.show ==='all')
+            return channel;
+          if(this.show === 'online' && channel.stream)
+            return channel;
+          if(this.show === 'offline' && !channel.stream)
+            return channel;
+          });
+    }
+  },
+  methods: {
+    doclick: function(){
+      console.log("click");
+    }
+  },
+  created() {
+
+    var streamNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    streamNames.forEach(function(channelName) {
+      axios.get(
+        "https://api.twitch.tv/kraken/channels/" + channelName,
+        { headers: { 'Client-ID': 'jdom04d5ik5g7ugrz6e41e2e7g89e3b' } }
+      )
+        .then(response => {
+          axios.get(
+            'https://api.twitch.tv/kraken/streams/' + response.data.name,
+            { headers: { 'Client-ID': 'jdom04d5ik5g7ugrz6e41e2e7g89e3b' } }
+          ).then(streamsResponse => {
+             this.channels.push({ 
+               data: response.data, 
+               name: response.data.name, 
+               stream: streamsResponse.data.stream  });
+          }).catch(e => console.log(e))
+        })
+        .catch(e => {
+          console.log(e);
+        })
+    }, this);
+  }
+}
+</script>
+<style lang='css'>
+@import '../node_modules/bulma/css/bulma.css';
+@import '../node_modules/bootstrap/dist/css/bootstrap.css';
+@import '../node_modules/font-awesome/css/font-awesome.css';
+</style>
+
+<style>
+
+  h2 {
+    font-size: 2em;
+    color: black;
+    margin-bottom: 15px;
+    margin-top: 15px;
+    letter-spacing: 1px;
+  }
+
+  article a {
+    color: inherit !important;
+    text-decoration: none !important;
+  }
+
+  .status-text {
+    font-size: .4em;
+    margin-left: .2em;
+  }
+
+  .status_icons {
+    /* margin botton, some lines make pretty */
+  }
+
+  .stream_img {
+    border-radius: 50%;
+    border: 1px solid black;
+  }
+
+  .centered {
+    text-align: center;
+  }
+
+  .online {
+    color: green;
+  }
+
+  .online :hover {
+    color: green;
+  }
+
+  .offline {
+    color: red;
+  }
+
+  .offline :hover {
+    color: red;
+  }
+
+  .both {
+    color: orange;
+  }
+
+  .both :hover {
+    color: orange;
+  }
+
+</style>
+
+<!-- 
+this.imgUrl = image;
+	this.name = name;  y
+	this.game = game; y
+	this.status = status; y   
+	this.light = color;  //need to get from status
+	this.type = type; //dont need - can infer from status y  -->  
---
+++ src/components/ChannelItem.vue
@@ -0,0 +1,60 @@
+<template>
+    <article class='media'>
+      <figure class="media-left">
+        <p class="image is-64x64">
+          <img :src="icon" class="stream_img img-responsive">
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong><a :href="'https://www.twitch.tv/' + channel.name">{{channel.name}}</a></strong> 
+            <small>{{channel.data.game}}</small>
+            <br> {{channel.data.status}}
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <i class="fa fa-circle"
+          :class="streamingStatus" aria-hidden="true"></i>
+      </div>
+    </article>
+</template>
+
+<script>
+
+export default {
+  name: 'ChannelItem',
+  props: ['channel'],
+  computed: 
+    {
+      streamingStatus : function(){
+          return this.channel.stream ? "online" : "offline"; 
+      },
+      icon : function(){
+        return  this.channel.data && this.channel.data.logo 
+        ? this.channel.data.logo
+        : 'https://s5.postimg.org/tgck4v97r/404a_prod_Not_Found_img.jpg';
+      }
+  }
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
